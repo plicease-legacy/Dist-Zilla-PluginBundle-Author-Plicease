ce32d2ae76924a5b67238a4ae6b72358cabe4cfc
Graham Ollis <perl@wdlabs.com>
2013-07-18 09:11:14 -0400

    merge UploadToMatrix and UploadToCPAN into one

---
+++ lib/Dist/Zilla/Plugin/Author/Plicease/Upload.pm
@@ -0,0 +1,45 @@
+package Dist::Zilla::Plugin::Author::Plicease::Upload;
+
+use Moose;
+use v5.10;
+
+# ABSTRACT: Upload dist to CPAN
+# VERSION
+
+extends 'Dist::Zilla::Plugin::UploadToCPAN';
+
+around release => sub {
+  my $orig = shift;
+  my $self = shift;
+  
+  if($self->zilla->chrome->prompt_yn("upload to CPAN?"))
+  {
+    eval { $self->$orig(@_) };
+    if(my $error = $@)
+    {
+      $self->zilla->log("error uploading to cpan: $error");
+      $self->zilla->log("you will have to manually upload the dist");
+    }
+  }
+  else
+  {
+    my($archive) = @_;
+    use autodie qw( :system );
+    my @cmd = ('scp', '-q', $archive, 'ollisg@matrix.wdlabs.com:web/sites/dist');
+    $self->zilla->log("% @cmd");
+    eval { system @cmd };
+    if(my $error = $@)
+    {
+      $self->zilla->log("NOTE SCP FAILED: $error");
+      $self->zilla->log("manual upload will be required");
+    }
+    else
+    {
+      $self->zilla->log("download URL: http://dist.wdlabs.com/$archive");
+    }
+  }
+  
+  return;
+};
+
+1;
--- t/use.t
+++ t/use.t
@@ -1,6 +1,6 @@
 use strict;
 use warnings;
-use Test::More tests => 11;
+use Test::More tests => 10;
 
 use_ok("Dist::Zilla::MintingProfile::Author::Plicease");
 use_ok("Dist::Zilla::Plugin::Author::Plicease::Init");
@@ -10,6 +10,5 @@ use_ok("Dist::Zilla::Plugin::Author::Plicease::PrePodWeaver");
 use_ok("Dist::Zilla::Plugin::Author::Plicease::Tests");
 use_ok("Dist::Zilla::Plugin::Author::Plicease::Thanks");
 use_ok("Dist::Zilla::Plugin::Author::Plicease::TransformTravis");
-use_ok("Dist::Zilla::Plugin::Author::Plicease::UploadToCPAN");
-use_ok("Dist::Zilla::Plugin::Author::Plicease::UploadToMatrix");
+use_ok("Dist::Zilla::Plugin::Author::Plicease::Upload");
 use_ok("Dist::Zilla::PluginBundle::Author::Plicease");
