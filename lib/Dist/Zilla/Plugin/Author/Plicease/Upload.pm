8285afe1696bb6127c87fab1a498945d896ffa4a
Graham Ollis <plicease@cpan.org>
2015-05-27 15:21:46 -0400

    updates to upload

--- lib/Dist/Zilla/Plugin/Author/Plicease/Upload.pm
+++ lib/Dist/Zilla/Plugin/Author/Plicease/Upload.pm
@@ -2,8 +2,50 @@ package Dist::Zilla::Plugin::Author::Plicease::Upload;
 
 use Moose;
 
-# ABSTRACT: Upload dist to CPAN
+# ABSTRACT: Upload a dist to CPAN
 # VERSION
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+This works similar to L<Dist::Zilla::Plugin::UploadToCPAN>.  Except:
+
+=over 4
+
+=item Non fatal
+
+It doesn't die if the upload does not succeed.  In my work flow I usually
+just upload the tarball manually when the upload fails.  Sometimes I also
+want to do the release step when I am not connected to the internet.
+
+=item Asks first
+
+It asks if you really want to upload to CPAN.  Some of my releases go to
+my server using C<scp> so if you either say no, or set C<cpan> to C<0>
+in the configuration it will do this instead.
+
+=back
+
+Basically just some personal preferences, you can and probably should
+replace this with C<[UploadToCPAN]> if you are taking over a dist.
+
+=head1 OPTIONS
+
+=head2 cpan
+
+Either C<1> or C<0>.  Set to C<0> and dist will not be uploaded to CPAN
+on release.
+
+=head2 scp_dest
+
+Valid C<scp> destination if CPAN upload is disabled.
+
+=head2 url
+
+Base web URL if CPAN upload is disabled.
+
+=cut
 
 extends 'Dist::Zilla::Plugin::UploadToCPAN';
 
@@ -12,13 +54,35 @@ has cpan => (
   default => sub { 1 },
 );
 
+has scp_dest => (
+  is      => 'ro',
+  default => sub { 'ollisg@matrix.wdlabs.com:web/sites/dist' },
+);
+
+has url => (
+  is      => 'ro',
+  default => sub { 'http://dist.wdlabs.com/' },
+);
+
+around before_release => sub {
+  my $orig = shift;
+  my $self = shift;
+  
+  # don't check username / password here
+  # do it during release
+};
+
 around release => sub {
   my $orig = shift;
   my $self = shift;
   
   if($self->cpan && $self->zilla->chrome->prompt_yn("upload to CPAN?"))
   {
-    eval { $self->$orig(@_) };
+    eval {
+      die "no username" unless length $self->username;
+      die "no password" unless length $self->password;
+      $self->$orig(@_);
+    };
     if(my $error = $@)
     {
       $self->zilla->log("error uploading to cpan: $error");
@@ -29,7 +93,7 @@ around release => sub {
   {
     my($archive) = @_;
     use autodie qw( :system );
-    my @cmd = ('scp', '-q', $archive, 'ollisg@matrix.wdlabs.com:web/sites/dist');
+    my @cmd = ('scp', '-q', $archive, $self->scp_dest);
     $self->zilla->log("% @cmd");
     eval { system @cmd };
     if(my $error = $@)
@@ -39,7 +103,7 @@ around release => sub {
     }
     else
     {
-      $self->zilla->log("download URL: http://dist.wdlabs.com/$archive");
+      $self->zilla->log("download URL: " . $self->url . "$archive");
     }
   }
   
