ebac8c3e102dc8629ee11bd28a2db7da99c12aaa
Graham Ollis <plicease@cpan.org>
2016-05-03 06:09:02 -0400

    bump requirement for [Author::Plicease]

--- lib/Dist/Zilla/PluginBundle/Author/Plicease.pm
+++ lib/Dist/Zilla/PluginBundle/Author/Plicease.pm
@@ -185,7 +185,7 @@ sub mvp_multivalue_args { qw(
 
 my %plugin_versions = qw(
   Alien                0.023
-  Author::Plicease.*   2.03
+  Author::Plicease.*   2.04
   OurPkgVersion        0.06
   MinimumPerl          1.006
   InstallGuide         1.200006
