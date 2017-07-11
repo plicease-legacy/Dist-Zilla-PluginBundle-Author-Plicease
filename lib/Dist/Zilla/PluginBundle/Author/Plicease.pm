d26420dd719b1d5682b3b71ce81397d79a4bffc1
Graham Ollis <plicease@cpan.org>
2017-07-11 05:06:53 -0400

    require 2.17

--- lib/Dist/Zilla/PluginBundle/Author/Plicease.pm
+++ lib/Dist/Zilla/PluginBundle/Author/Plicease.pm
@@ -179,7 +179,7 @@ sub mvp_multivalue_args { qw(
 
 my %plugin_versions = qw(
   Alien                0.023
-  Author::Plicease.*   2.16
+  Author::Plicease.*   2.17
   OurPkgVersion        0.12
   MinimumPerl          1.006
   InstallGuide         1.200006
