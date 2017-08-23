dc4ea3adfa246b8c6f236c98e8fc938dadfe3c1a
Graham Ollis <plicease@cpan.org>
2017-08-23 15:10:29 -0400

    require 2.22 plugins

--- lib/Dist/Zilla/PluginBundle/Author/Plicease.pm
+++ lib/Dist/Zilla/PluginBundle/Author/Plicease.pm
@@ -178,7 +178,7 @@ Specify a minimum Perl version.  If not specified it will be detected.
 
   my %plugin_versions = qw(
     Alien                0.023
-    Author::Plicease.*   2.21
+    Author::Plicease.*   2.22
     OurPkgVersion        0.12
     MinimumPerl          1.006
     InstallGuide         1.200006
